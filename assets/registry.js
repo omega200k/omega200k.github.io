ff621835e1b2210612bcf1f99e84458a67b5f736
omega200k <omegabomba2000k@proton.me>
2025-07-13 19:54:42 +0800

    Update registry.js

--- assets/registry.js
+++ assets/registry.js
@@ -5,6 +5,6 @@ function login () {
     if (userID === 'admin' && password === '1234') {
         window.location.href = 'homepage.html';
     } else {
-        alert('Invalid username or password');
+        alert('Invalid username or password, try user: admin, pass: 1234');
     }
 }
