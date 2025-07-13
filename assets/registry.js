f34ea1df1fbcb7971b685cbda6924e139f2e1fd8
omega200k <omegabomba2000k@proton.me>
2025-07-13 17:15:50 +0800

    Update registry.js

--- assets/registry.js
+++ assets/registry.js
@@ -1,9 +1,10 @@
-function login (){
-const userID = document.getElementById('userID');
-if (userdID === 'admin'){
-window.location.href = 'homepage.html';
-}else{
-alert('Wrong User');
-}
-
+function login () {
+    const userID = document.getElementById('userID').value;
+    const password = document.getElementById('passwordcreation').value;
+  
+    if (userID === 'admin' && password === '1234') {
+        window.location.href = 'homepage.html';
+    } else {
+        alert('Invalid username or password');
+    }
 }
